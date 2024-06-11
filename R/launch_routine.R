579de4765b57166b1c7e7eb5940daad340d00801
pkostyrka <pierre.kostyrka@orange.fr>
2024-06-11 14:23:39 +0200

    Global function

---
+++ R/launch_routine.R
@@ -0,0 +1,131 @@
+
+#' launch_routine
+#'
+#' @return A routine based on functions of the ecotaxatools package.
+#' @export
+#'
+#' @examples
+launch_routine <- function() {
+  # Definition de l'instrument
+  instru <- dlg_list(c("PlanktoScope"), title="Instrument")$res
+
+  # Fichiers (faire option selection seulement certains fichiers?)
+  start <- dlg_message("Please select the data directory. All the tsv files in this directory will be processed.", type="okcancel")$res
+  if (start=="cancel") stop("Canceled.")
+
+  if (exists('utils::choose.dir')) {
+    mainpath <- utils::choose.dir()
+    path <- mainpath %>% list.files(".tsv", full.names = T)
+  } else {
+    mainpath <- tcltk::tk_choose.dir()
+    path <- mainpath %>% list.files(".tsv", full.names = T)
+  }
+
+  # Create a new directory for created files
+  if (!file.exists(file.path(mainpath, paste0("new_",format(Sys.time(), "%d-%b-%Y %H.%M"))))) {
+    dir.create(file.path(mainpath, paste0("new_",format(Sys.time(), "%d-%b-%Y %H.%M"))))
+  }
+  output <- file.path(mainpath, paste0("new_",format(Sys.time(), "%d-%b-%Y %H.%M")))
+
+
+  # COMPUTE DATA
+  # ------------------------------------------------------------------------------
+  # Check metadata
+  check_metadata(path, output)
+
+  # Compute biovolumes and resume to BSS (warning : not normalized by size class)
+  yesno <- dlg_message("Do you want to select a metadata table ? If not the original metadata will be used.", type="yesno")$res
+
+  if(yesno=="yes") {
+    metadata <- file.choose() %>% read_csv2()
+    bss <- lapply(path, function(x) BSS_table(compute_bv(x, output, metadata))) %>% bind_rows()
+  } else {
+    bss <- lapply(path, function(x) BSS_table(compute_bv(x, output))) %>% bind_rows()
+  }
+
+
+  # RESUME DATA AND SAVING TABLES
+  # ------------------------------------------------------------------------------
+  # Create directory
+  if (!file.exists(file.path(output,"resumes"))) {
+    dir.create(file.path(output,"resumes"))
+  }
+  path.resume <- file.path(output,"resumes")
+
+  # Global resume table without size class
+  res <- bss %>% group_by(sample_id, object_annotation_category, type) %>%
+    summarize(AB = sum(AB, na.rm=T),
+              BV= sum(BV, na.rm=T))
+
+  # Loop to save tables for Biovolume (Elli/Plain/Riddled)
+  for (bv.type in unique(res$type)) {
+    t <- res %>% filter(type==bv.type) %>% select(-type, -AB) %>%
+      pivot_wider(names_from = sample_id,
+                  values_from = BV)
+    t$Total <- rowSums(t[-1], na.rm=T) # sum by sp
+    tot <- t[1,] %>% mutate(object_annotation_category="Total") # sum by sample
+    tot[-1] <- t(colSums(t[-1], na.rm=T))
+    t <- rbind(t, tot)
+    write_csv2(t, file.path(path.resume, paste0("BV_",bv.type,".csv")))
+  }
+
+  # Resume table for Abundance (same for elli, plain, etc.)
+  t2 <- res %>% filter(type=="plain") %>% select(-type, -BV) %>%
+    pivot_wider(names_from = sample_id,
+                values_from = AB)
+  t$Total <- rowSums(t[-1], na.rm=T) # sum by sp
+  tot <- t[1,] %>% mutate(object_annotation_category="Total") # sum by sample
+  tot[-1] <- t(colSums(t[-1], na.rm=T))
+  t <- rbind(t, tot)
+  write_csv2(t, file.path(path.resume, "AB.csv"))
+
+  # Unite taxonomiques
+  taxo <- add.taxo(unique(bss$object_annotation_hierarchy)) %>% add.zoo
+
+  # Search for metadata if not loaded already
+  if(!exists("metadata")){
+    metadata <- read_csv2(file.path(output, "metadata", "original_metadata.csv"))
+  }
+
+  # Saving tables
+  write_csv2(metadata, file.path(path.resume, "metadata_used.csv"))
+  write_csv2(bss, file.path(path.resume, "BSS.csv"))
+  write_csv2(res, file.path(path.resume, "resume_all.csv"))
+  write_csv2(taxo, file.path(path.resume, "taxo.csv"))
+
+
+  # GRAPHICAL OUPUTS
+  # ------------------------------------------------------------------------------
+  # Create directory
+  if (!file.exists(file.path(output,"graph"))) {
+    dir.create(file.path(output,"graph"))
+  }
+  path.graph <- file.path(output,"graph")
+
+  # for the original metadata
+  pdf(file.path(path.graph, "original_metadata.pdf"), paper="a4r")
+  graph.metadata(read_csv2(file.path(output, "metadata", "original_metadata.csv")))
+  dev.off()
+
+  # for the edited metadata
+  pdf(file.path(path.graph, "metadata.pdf"), paper="a4r")
+  graph.metadata(metadata)
+  dev.off()
+  # system(paste0('open "', file.path(path.graph, "metadata.pdf"), '"'))
+
+  # for the project
+  pdf(file.path(path.graph, "graph_project.pdf"), paper="a4r")
+  graph.project(bss, metadata, taxo)
+  dev.off()
+
+  # for each sample
+  for (i in unique(bss$sample_id)) {
+    bss %>% filter(sample_id==i) %>% graph.sample(metadata, taxo) %>%
+      ggsave(filename=file.path(path.graph, paste0(i,".jpg")),
+             width=297, height=210, units = "mm")
+  }
+
+
+
+  print("End of the script.")
+}
